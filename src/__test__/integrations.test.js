8100318c68792721856209312d45ea53304dc5cc
Allen <allen0321.lin@ehsn.com.tw>
2020-11-24 18:26:34 +0800

    添加 Fetch API 以及 整合測試

---
+++ src/__test__/integrations.test.js
@@ -0,0 +1,37 @@
+﻿import React from "react";
+import { mount } from "enzyme";
+import moxios from "moxios";
+import Root from "Root";
+import App from "components/App";
+
+let wrapped;
+
+beforeEach(() => {
+  moxios.install();
+
+  moxios.stubRequest("http://jsonplaceholder.typicode.com/comments", {
+    status: 200,
+    response: [{ name: "Fetched #1" }, { name: "Fetched #2" }],
+  });
+
+  wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+});
+
+afterEach(() => {
+  wrapped.unmount();
+  moxios.uninstall();
+});
+
+it("can fetch a list of comments and display them", done => {
+  wrapped.find(".fetch-comments").simulate("click");
+
+  moxios.wait(() => {
+    wrapped.update();
+    expect(wrapped.find("li").length).toEqual(2);
+    done();
+  });
+});
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -24,7 +24,9 @@ class ComponentBox extends React.Component {
             <button>Submit Comment</button>
           </div>
         </form>
-        <button onClick={this.props.fetchComments}>Fetch Comment</button>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>
+          Fetch Comment
+        </button>
       </div>
     );
   }
